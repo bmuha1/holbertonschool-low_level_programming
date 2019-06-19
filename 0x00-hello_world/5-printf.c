c3b0ebe08cbdd96704632c3fbfe788527eb9559d
Brent Muha <800@holbertonschool.com>
2019-06-19 19:46:39 +0000

    Add printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+ * main - print a string using printf
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
