3641936981aac1684caf7c143ff4385c5a2778a2
Brent Muha <800@holbertonschool.com>
2019-06-20 16:47:25 +0000

    Add three digit combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Print combinations of three digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int hundreds;
+	int tens;
+	int ones;
+
+	for (hundreds = 0; hundreds <= 9; hundreds++)
+	{
+		for (tens = hundreds + 1; tens <= 9; tens++)
+		{
+			for (ones = tens + 1; ones <= 9; ones++)
+			{
+				putchar(hundreds + '0');
+				putchar(tens + '0');
+				putchar(ones + '0');
+
+				if (hundreds < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
