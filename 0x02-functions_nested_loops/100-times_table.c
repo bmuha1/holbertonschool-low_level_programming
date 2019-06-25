7703d33dae853a6c1d1df2f0ccba2add889c0bda
Brent Muha <800@holbertonschool.com>
2019-06-25 16:11:10 +0000

    Fix times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,49 +16,33 @@ void print_times_table(int n)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			if (j == 0)
+			if ((i * j) < 10)
 			{
-				_putchar('0');
-
-				if (n != 0)
+				if (j != 0)
 				{
-					_putchar(',');
+					_putchar(' ');
 					_putchar(' ');
 				}
-				else
-				{
-					_putchar('\n');
-				}
+			}
+			else if ((i * j) < 100)
+			{
+				_putchar(' ');
+				_putchar(i * j / 10 + '0');
 			}
 			else
 			{
-				if ((i * j) < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else if ((i * j) < 100)
-				{
-					_putchar(' ');
-					_putchar(i * j / 10 + '0');
-				}
-				else
-				{
-					_putchar(i * j / 100 + '0');
-					_putchar((i * j / 10) % 10 + '0');
-				}
-
-				_putchar(i * j % 10 + '0');
-
-				if (j == n)
-				{
-					_putchar('\n');
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar(i * j / 100 + '0');
+				_putchar((i * j / 10) % 10 + '0');
+			}
+			_putchar(i * j % 10 + '0');
+			if (j == n)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 	}
