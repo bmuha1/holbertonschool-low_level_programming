c16fbb7e38b16273ad29084ecc9aa4263e662b39
Brent Muha <800@holbertonschool.com>
2019-06-25 13:19:35 +0000

    Fix print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,13 +9,7 @@
 int print_last_digit(int n)
 {
 	if (n < 0)
-	{
-		_putchar((-n) % 10 + '0');
-		return ((-n) % 10);
-	}
-	else
-	{
-		_putchar(n % 10 + '0');
-		return (n % 10);
-	}
+		n = -n;
+	_putchar(n % 10 + '0');
+	return (n % 10);
 }
