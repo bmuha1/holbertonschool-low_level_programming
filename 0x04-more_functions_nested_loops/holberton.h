9625c93de2ed9d47340e9c811fbba38e55af6f73
Brent Muha <800@holbertonschool.com>
2019-06-27 19:08:05 +0000

    Add extra functions to holberton.h

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -12,5 +12,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+int prime(int p);
+void print_number(int n);
 
 #endif /* _HOLBERTON_H_ */
