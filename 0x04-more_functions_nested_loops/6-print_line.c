8a91f41c5edde96d314b336b646d9eb5d59fb641
Brent Muha <800@holbertonschool.com>
2019-06-27 16:39:50 +0000

    Add print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_line(int n) - Print a straight line using underscores
+ * @n: The number of underscores to print
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+		_putchar('_');
+
+	_putchar('\n');
+}
