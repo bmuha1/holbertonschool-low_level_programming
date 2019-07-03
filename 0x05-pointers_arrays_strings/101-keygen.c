7c1fbca95a03da1449d15bbf6bf0f3f2cfd5ee2a
Brent Muha <800@holbertonschool.com>
2019-07-03 06:29:14 +0000

    Fix keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,16 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - Generate the password to 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int i;
-	char c[10];
+	int i, offset, total;
+	char c[50] = "";
+	char letters[] = "BMTc";
+
 	srand(time(NULL));
 
-	for (i = 0; i < 10; i++)
-		c[i] = 33 + rand() % 94;
+	total = 0;
+	offset = rand() % 4;
 
-	printf("%s\n", c);
+	for (i = 0; total < 2772; i++)
+	{
+		c[i] = letters[offset];
+		total += c[i];
+	}
+	c[i] = '\0';
+	printf("%s", c);
 
 	return (0);
 }
