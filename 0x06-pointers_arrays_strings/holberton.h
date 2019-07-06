ae5e81fd5418ccbaf3905fbf464ed1bbc20473d1
Brent Muha <800@holbertonschool.com>
2019-07-06 01:21:30 +0000

    Add functions to header

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -14,5 +14,6 @@ char *rot13(char *s);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+void print_line(char *b, int position, int n);
 
 #endif
