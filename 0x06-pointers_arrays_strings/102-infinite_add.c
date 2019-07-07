6f8e907a40de67b5cb1c83e75c526893f78cd668
Brent Muha <800@holbertonschool.com>
2019-07-07 15:37:05 +0000

    Fix infinite add

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -17,7 +17,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	size_n2 = pos2 = _strlen(n2);
 
 	remainder = 0;
-	for (i = size_r; i >= 0; i--)
+	for (i = size_r; i > 0; i--)
 	{
 		tmp1 = tmp2 = 0;
 		if (n1[pos1] >= '0' && n1[pos1] <= '9' && pos1 >= 0)
@@ -25,7 +25,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (n2[pos2] >= '0' && n2[pos2] <= '9' && pos2 >= 0)
 			tmp2 = n2[pos2] - '0';
 
-		r[i] = (tmp1 + tmp2 + remainder) % 10 + '0';
+		r[i - 1] = (tmp1 + tmp2 + remainder) % 10 + '0';
 
 		if (tmp1 + tmp2 + remainder > 9)
 			remainder = 1;
@@ -35,10 +35,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		pos1--;
 		pos2--;
 	}
-	r[size_r] = '\0';
+	r[size_r - 1] = '\0';
 
 	if ((size_r <= size_n1) || (size_r <= size_n2) ||
-	    (r[0] > '0' && (size_r <= size_n1 + 1 || size_r <= size_n2 + 1)))
+	    (remainder && (size_r == size_n1 + 1 || size_r == size_n2 + 1)))
 		return (0);
 
 	i = 0;
@@ -46,9 +46,9 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		i++;
 
 	if (r[i] == '\0')
-		return (&r[i - 1]);
-
-	return (&r[i]);
+		return (r);
+	else
+		return (&r[i]);
 }
 
 /**
@@ -59,11 +59,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
  */
 int _strlen(char *s)
 {
-	int i, length;
+	int i;
 
-	length = 0;
 	for (i = 0; s[i] != '\0'; i++)
-		length++;
+		;
 
-	return (length);
+	return (i);
 }
