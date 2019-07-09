dc107bee0c78a58d2b5b48c8b1aabdd7f3397300
Brent Muha <800@holbertonschool.com>
2019-07-09 20:12:58 +0000

    Fix strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,11 +9,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
 
-	for (i = 0; s[i] != '\0'; i++)
-		if (s[i] == c)
-			return (&s[i]);
+		s++;
+	}
 
 	return (0);
 }
