4fb23417b4c74073ef059f6ffd015b3a7e80271b
Brent Muha <800@holbertonschool.com>
2019-07-10 18:19:23 +0000

    Add print rev recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - Print a string in reverse recursively
+ * @s: The string to print
+ */
+void _print_rev_recursion(char *s)
+{
+	s++;
+	if (s[0] != '\0')
+		_print_rev_recursion(s);
+
+	_putchar(s[-1]);
+}
