044c46b1ae546cf32785e147f8e0ab7b922bc3e5
Brent Muha <800@holbertonschool.com>
2019-07-11 03:20:34 +0000

    Fix wildcmp

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -33,29 +33,16 @@ int check_wildcmp(char *s1, char *s2, int l1, int l2, int c1, int c2)
 	if (c1 >= l1 && c2 >= l2)
 		return (1);
 
-	if (s1[c1] == '*')
-	{
-		if (s1[c1 + 1] == '*')
-			return (check_wildcmp(s1, s2, l1, l2, c1 + 1, c2));
-		else if (s1[c1 + 1] == s2[c2])
-			return (check_wildcmp(s1, s2, l1, l2, c1 + 2, c2 + 1));
-		else
-			return (check_wildcmp(s1, s2, l1, l2, c1, c2 + 1));
-	}
-	else if (s2[c2] == '*')
-	{
-		if (s2[c2 + 1] == '*')
-			return (check_wildcmp(s1, s2, l1, l2, c1, c2 + 1));
-		else if (s2[c2 + 1] == s1[c1])
-			return (check_wildcmp(s1, s2, l1, l2, c1 + 1, c2 + 2));
-		else
-			return (check_wildcmp(s1, s2, l1, l2, c1 + 1, c2));
-	}
-
 	if (s1[c1] == s2[c2])
 		return (check_wildcmp(s1, s2, l1, l2, c1 + 1, c2 + 1));
-	else
-		return (0);
+
+	if (s1[c1] == '*' || s2[c2] == '*')
+	{
+		return (check_wildcmp(s1, s2, l1, l2, c1 + 1, c2) ||
+			check_wildcmp(s1, s2, l1, l2, c1, c2 + 1));
+	}
+
+	return (0);
 }
 
 /**
