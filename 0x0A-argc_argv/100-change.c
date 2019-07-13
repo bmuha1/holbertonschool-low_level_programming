985156d4950ba1b9c20fc30cb791eabf1b02d80f
Brent Muha <800@holbertonschool.com>
2019-07-13 14:46:27 +0000

    Improve change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -67,29 +67,17 @@ int number_of_coins(int cents)
 {
 	int coins = 0;
 
-	if (cents / 25)
-	{
-		coins += (cents / 25);
-		cents -= (25 * (cents / 25));
-	}
+	coins += (cents / 25);
+	cents %= 25;
 
-	if (cents / 10)
-	{
-		coins += (cents / 10);
-		cents -= (10 * (cents / 10));
-	}
+	coins += (cents / 10);
+	cents %= 10;
 
-	if (cents / 5)
-	{
-		coins += (cents / 5);
-		cents -= (5 * (cents / 5));
-	}
+	coins += (cents / 5);
+	cents %= 5;
 
-	if (cents / 2)
-	{
-		coins += (cents / 2);
-		cents -= (2 * (cents / 2));
-	}
+	coins += (cents / 2);
+	cents %= 2;
 
 	coins += cents;
 
