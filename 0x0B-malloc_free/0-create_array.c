84055984c638c0f3b98e97c5ec7bcb0fef3f2836
Brent Muha <800@holbertonschool.com>
2019-07-16 19:10:12 +0000

    Fix create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,17 +13,16 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *ar;
 
-	if (size <= 0)
+	if (size == 0)
 		return (NULL);
 
 	ar = malloc(sizeof(char) * size + 1);
+	if (ar == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		ar[i] = c;
 	ar[size] = '\0';
-
-	if (ar == NULL)
-		return (NULL);
 
 	return (ar);
 }
