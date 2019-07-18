ba3e50e2ed583ebc6d4f8db604918e2f46dedad9
Brent Muha <800@holbertonschool.com>
2019-07-18 20:47:32 +0000

    Update header

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -9,5 +9,7 @@ char *_memset(char *s, char b, unsigned int n);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *_memcpy(char *dest, char *src, unsigned int n);
+int _putchar(char c);
+int is_positive_number(char *number);
 
 #endif
