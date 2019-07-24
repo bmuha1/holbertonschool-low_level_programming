91aaa8ef7527ed14827b6e43db8577eafa45f342
Brent Muha <800@holbertonschool.com>
2019-07-24 19:54:30 +0000

    Update main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,12 +18,6 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if ((*get_op_func)(argv[2]) == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && (atoi(argv[3]) == 0))
 	{
 		printf("Error\n");
