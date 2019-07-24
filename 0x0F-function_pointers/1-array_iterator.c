68297ffbf30867a59dcc1710077372df7f34dd7d
Brent Muha <800@holbertonschool.com>
2019-07-24 19:18:59 +0000

    Fix array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
+	if ((*action) == NULL)
+		return;
+
+	if (size <= 0)
+		return;
+
 	for (i = 0; i < size; i++)
 		(*action)(array[i]);
 }
