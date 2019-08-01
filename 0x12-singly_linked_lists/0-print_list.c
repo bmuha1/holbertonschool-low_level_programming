dd2b27da5c88979748dbd3ed8ea451e048fbb26f
Brent Muha <800@holbertonschool.com>
2019-08-01 17:00:27 +0000

    Add print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	size_t size = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		if (h->str)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+
+		h = h->next;
+		size++;
+	}
+
+	return (size);
+}
