89f353247a460aeb4248b9e068b8f4e8b53b0245
Brent Muha <800@holbertonschool.com>
2019-08-05 19:34:57 +0000

    Fix description

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * free_listint - Free a listint_t list and set the head to NULL
+ * free_listint2 - Free a listint_t list and set the head to NULL
  * @head: The list
  */
 void free_listint2(listint_t **head)
