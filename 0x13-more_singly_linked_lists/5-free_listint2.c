f46b772e89f531e597528cf542a6ccfc89edbaea
Brent Muha <800@holbertonschool.com>
2019-08-05 18:07:57 +0000

    Add free list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - Free a listint_t list and set the head to NULL
+ * @head: The list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+
+	*head = NULL;
+}
