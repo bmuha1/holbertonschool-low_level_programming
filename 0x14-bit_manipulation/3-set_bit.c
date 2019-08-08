82799e96c47b25e7a792a80374c677f64feda82d
Brent Muha <800@holbertonschool.com>
2019-08-08 20:55:43 +0000

    Add error case

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,7 +9,11 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	*n |= 1UL << index;
-
-	return (1);
+	if (index < (8 * sizeof(unsigned long int)))
+	{
+		*n |= 1UL << index;
+		return (1);
+	}
+	else
+		return (-1);
 }
