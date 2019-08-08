f7ff6129343c5d8c7c826a22e88c3001881619b7
Brent Muha <800@holbertonschool.com>
2019-08-08 19:25:35 +0000

    Fix print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,17 +7,38 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int i;
+	int start_printing = 0;
 
 	if (n == 0)
 		_putchar('0');
 	else
 	{
-		for (i = n; i > 0; i >>= 2)
+		for (i = 1UL << 63; i > 0; i >>= 1)
 		{
 			if (n & i)
+			{
 				_putchar('1');
-			else
+				start_printing = 1;
+			}
+			else if (start_printing)
 				_putchar('0');
 		}
 	}
 }
+
+/*
+
+        else
+        {
+                for (i = (unsigned long) 1 << 63; i > 0; i /= 2)
+                {
+                        if (n & i)
+                        {
+                                _putchar('1');
+                                start_printing = 1;
+                        }
+                        else if (start_printing)
+                                _putchar('0');
+                }
+        }
+*/
