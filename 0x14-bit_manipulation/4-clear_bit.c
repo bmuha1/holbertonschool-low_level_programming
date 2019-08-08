1dd4f450289fea262d127c833f5c2c20b5cb913f
Brent Muha <800@holbertonschool.com>
2019-08-08 20:58:04 +0000

    Add error case

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,7 +9,11 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	*n &= ~(1 << index);
-
-	return (1);
+	if (index < (8 * sizeof(unsigned long int)))
+	{
+		*n &= ~(1 << index);
+		return (1);
+	}
+	else
+		return (-1);
 }
