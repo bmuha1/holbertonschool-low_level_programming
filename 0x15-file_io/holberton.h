5bdb6e0ef8bfa7fe61ff416db6d37a2e6a1ded42
Brent Muha <800@holbertonschool.com>
2019-08-13 18:07:47 +0000

    Update header

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -13,6 +13,11 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+void args_error(void);
+void file_from_error(char *file);
+void file_to_error(char *file);
+void close_error(int fd);
+
 int _putchar(char c);
 int _strlen(char *s);
 
