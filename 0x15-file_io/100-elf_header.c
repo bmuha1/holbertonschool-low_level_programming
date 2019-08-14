ec32107b423d04caa624222d432e015ab6e96203
Brent Muha <800@holbertonschool.com>
2019-08-14 03:25:13 +0000

    Add elf header frame

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -14,7 +15,7 @@
 int main(int argc, char *argv[])
 {
 	int fd;
-	char buf[1024];
+	Elf64_Ehdr *header;
 	ssize_t size;
 
 	if (argc != 2)
@@ -22,7 +23,6 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
 		exit(98);
 	}
-
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
@@ -30,18 +30,36 @@ int main(int argc, char *argv[])
 			argv[1]);
 		exit(98);
 	}
-
-	size = read(fd, buf, 1024);
+	header = malloc(sizeof(Elf64_Ehdr));
+	if (!header)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't allocate memory\n");
+		exit(98);
+	}
+	size = read(fd, header, sizeof(Elf64_Ehdr));
 	if (size == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 			argv[1]);
 		exit(98);
 	}
+	if (memcmp((*header).e_ident, ELFMAG, SELFMAG) == 0)
+		print_elf();
+	else
+	{
+		dprintf(STDERR_FILENO, "Error: not a valid ELF file");
+		exit(98);
+	}
+	close(fd);
+	return (0);
+}
 
+/**
+ * print_elf - Print ELF header information
+ */
+void print_elf(void)
+{
 	printf("ELF Header:\n");
 	printf("  Magic:\n  Class:\n  Data:\n  Version:\n  OS/ABI:\n");
 	printf("  ABI Version:\n  Type:\n  Entry point address:\n");
-
-	return (0);
 }
