396eb05262e3a55160186eec3f4e7d342d2b46b0
Brent Muha <800@holbertonschool.com>
2019-09-14 19:45:39 +0000

    Add NULL head check

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,6 +14,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new;
 	unsigned int i;
 
+	if (!h)
+		return (NULL);
 	if (*h == NULL && idx != 0)
 		return (NULL);
 
