e8243891f592610aae0e31210a124ceb84067349
Brent Muha <800@holbertonschool.com>
2019-10-08 06:10:52 +0000

    Add hash table create

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new = malloc(sizeof(hash_table_t));
+	if (!new)
+		return NULL;
+
+	new->array = malloc(size * sizeof(hash_node_t *));
+	if (!new)
+	{
+		free(new);
+		return NULL;
+	}
+
+	return (new);
+}
