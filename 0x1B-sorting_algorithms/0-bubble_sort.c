c843aab248b5929c91a7d18184c4b8c26e23216c
Brent Muha <800@holbertonschool.com>
2019-10-12 00:10:40 +0000

    Add bubble sort

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sort integers using the Bubble sort algorithm
+ * @array: The array of integers
+ * @size: The number of elements
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i;
+	int swapped, temp;
+
+	do {
+		swapped = 0;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = temp;
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+	} while (swapped);
+}
