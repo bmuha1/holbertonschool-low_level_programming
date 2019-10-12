5a3a036dbfdf7605c1a6508468929432f4fc44b1
Brent Muha <800@holbertonschool.com>
2019-10-12 22:32:09 +0000

    Move printing to other function

--- 0x1B-sorting_algorithms/103-merge_sort.c
+++ 0x1B-sorting_algorithms/103-merge_sort.c
@@ -13,19 +13,50 @@ void merge_sort(int *array, size_t size)
 	merge_recursive(array, size, 0, size - 1);
 }
 
+/**
+ * merge_recursive - Merge sort recursively
+ * @array: The array of integers
+ * @size: The number of elements
+ * @l: The left-most index
+ * @r: The right-most index
+ */
 void merge_recursive(int *array, size_t size, int l, int r)
 {
-	int m;
+	int i, m;
 
 	if (l < r)
 	{
 		m = (l + r - 1) / 2;
 		merge_recursive(array, size, l, m);
 		merge_recursive(array, size, m + 1, r);
+
+		printf("Merging...\n[left]: ");
+		for (i = l; i <= m; i++)
+		{
+			printf("%d", array[i]);
+			if (i < m)
+				printf(", ");
+		}
+		printf("\n[right]: ");
+		for (i = m + 1; i <= r; i++)
+		{
+			printf("%d", array[i]);
+			if (i < r)
+				printf(", ");
+		}
+
 		merge(array, size, l, m, r);
 	}
 }
 
+/**
+ * merge - Merge two lists together
+ * @array: The array of integers
+ * @size: The number of elements
+ * @l: The left-most index
+ * @m: The middle index
+ * @r: The right-most index
+ */
 void merge(int *array, size_t size, int l, int m, int r)
 {
 	int i, j, k;
@@ -34,28 +65,10 @@ void merge(int *array, size_t size, int l, int m, int r)
 	temp = malloc(size * sizeof(int));
 	if (!temp)
 		return;
-
 	i = l;
 	j = m + 1;
 	k = 0;
 
-	printf("Merging...\n[left]: ");
-	for (i = l; i <= m; i++)
-	{
-		printf("%d", array[i]);
-		if (i < m)
-			printf(", ");
-	}
-
-	printf("\n[right]: ");
-	for (i = m + 1; i <= r; i++)
-	{
-		printf("%d", array[i]);
-		if (i < r)
-			printf(", ");
-	}
-
-	i = l;
 	while (i <= m && j <= r)
 	{
 		if (array[i] < array[j])
