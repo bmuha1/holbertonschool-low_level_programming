e0f5cdb9ce2f59c2379d86cb93eb1e5a0920ba42
Brent Muha <800@holbertonschool.com>
2019-12-02 21:15:11 +0000

    Fix left_height and right_height off by one error

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -8,13 +8,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	int left_height = 0, right_height = 0;
 
 	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
-	left_height = (1 + binary_tree_height(tree->left));
-	right_height = (1 + binary_tree_height(tree->right));
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
 
 	if (left_height > right_height)
 		return (left_height);
