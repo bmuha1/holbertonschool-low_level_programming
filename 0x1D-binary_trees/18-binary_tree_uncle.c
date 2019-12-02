bf718b54242b40c7002e9f0c03e8f21583017dfa
Brent Muha <800@holbertonschool.com>
2019-12-02 22:51:22 +0000

    Add uncle

---
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Find the uncle of a node
+ * @node: Pointer to the node
+ *
+ * Return: Pointer to the uncle, or NULL if no uncle
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
+
+	return (binary_tree_sibling(node->parent));
+}
+
+/**
+ * binary_tree_sibling - Find the sibling of a node
+ * @node: Pointer to the node
+ *
+ * Return: Pointer to the sibling, or NULL if no sibling
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
