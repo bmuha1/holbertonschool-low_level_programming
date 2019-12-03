3a771a129aeeacdc204cfa9d5960d2139d518b14
Brent Muha <800@holbertonschool.com>
2019-12-03 21:10:31 +0000

    Add bst support

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -21,3 +21,24 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	return (new);
 }
+
+/**
+ * bst_node - Create a binary search tree node
+ * @value: The value to put in the new node
+ *
+ * Return: A pointer to the new node, or NULL on failure
+ */
+bst_t *bst_node(int value)
+{
+	bst_t *new = malloc(sizeof(bst_t));
+
+	if (!new)
+		return (NULL);
+
+	new->n = value;
+	new->parent = NULL;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
+}
