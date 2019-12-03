f8e31b1958d25c184c1fda91b400972a0298eba2
Brent Muha <800@holbertonschool.com>
2019-12-03 20:23:25 +0000

    Add is bst

---
+++ 0x1D-binary_trees/110-binary_tree_is_bst.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_bst - Check if a binary tree is a binary search tree
+ * @tree: Pointer to the root node
+ *
+ * Return: 1 if it's a valid BST, 0 otherwise
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	binary_tree_t *prev = NULL;
+
+	if (!tree)
+		return (0);
+
+	return (binary_tree_is_bst_helper((binary_tree_t *)tree, prev));
+}
+
+/**
+ * binary_tree_is_bst_helper - Check if a binary tree is a binary search tree
+ * @tree: Pointer to the root node
+ * @prev: Pointer to the previous node
+ *
+ * Return: 1 if it's a valid BST, 0 otherwise
+ */
+int binary_tree_is_bst_helper(binary_tree_t *tree, binary_tree_t *prev)
+{
+	if (tree)
+	{
+		if (!binary_tree_is_bst_helper(tree->left, prev))
+			return (0);
+		if (prev != NULL && tree->n <= prev->n)
+			return (0);
+		prev = tree;
+
+		return (binary_tree_is_bst_helper(tree->right, prev));
+	}
+
+	return (1);
+}
