cb42eb93f68cd2d0eb485ff430ea9b639af549ec
Brent Muha <800@holbertonschool.com>
2020-01-14 18:04:42 +0000

    Add linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Search for the value in an array of integers
+ * @array: Pointer to the first element of the array to search
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ *
+ * Return: First index where value is located, or -1 if it's not there
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int i;
+
+	if (!array)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
